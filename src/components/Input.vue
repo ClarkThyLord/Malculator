9f4ab5f230d96cec3b6f813f7f991bce541c14d6
Christian Cuevas <ccpotential@gmail.com>
2019-11-15 17:05:51 -0800

    fix up Input
    Clark was here :V

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,39 +1,63 @@
 <template>
-    <div>
-        <form @submit.prevent="process">
-            <input v-model="func" placeholder="function here..." />
-            <input v-model="A" placeholder="add A"/>
-            <input v-model="B" placeholder="add B"/>
-            <input v-model="N" placeholder="add N"/>
-            <button type="submit">Enviar</button>
-        </form>     
+  <form class="uk-flex" @submit.prevent="process">
+    <div class="uk-margin-right">
+      <label class="uk-form-label" for="form-stacked-text">Function \ Función:</label>
+      <div class="uk-form-controls">
+        <input type="text" v-model="func" placeholder="Function..." class="uk-input" />
+      </div>
     </div>
+    <div class="uk-margin-right">
+      <label class="uk-form-label" for="form-stacked-text">Starting Range \ Rango de Inicio: (A)</label>
+      <div class="uk-form-controls">
+        <input type="number" v-model="A" placeholder="Starting range..." class="uk-input" />
+      </div>
+    </div>
+    <div class="uk-margin-right">
+      <label class="uk-form-label" for="form-stacked-text">Ending Range \ Rango de Finalización: (B)</label>
+      <div class="uk-form-controls">
+        <input type="number" v-model="B" placeholder="Ending range..." class="uk-input" />
+      </div>
+    </div>
+    <div class="uk-margin-right">
+      <label class="uk-form-label" for="form-stacked-text">Iterations \ Iteraciones: (N)</label>
+      <div class="uk-form-controls">
+        <input type="number" v-model="N" placeholder="Iteration count..." class="uk-input" />
+      </div>
+    </div>
+    <button type="submit" class="uk-button uk-button-default">Calculate</button>
+  </form>
 </template>
 
 <script>
 import App from "../App.vue";
 
 export default {
-    data: function (){
-        return {
-            func: '',
-            A: '',
-            B: '',
-            N: ''
-        }
-    }, 
-    methods: {
-        process: function () {
-            console.log(window.integral.trap(window.mathjs.evaluate(this.A), window.mathjs.evaluate(this.B), window.mathjs.evaluate(this.N), window.mathjs.evaluate(this.func)));
-            console.log(this.A);
-            console.log(this.B);
-            console.log(this.N);
-        }
+  data: function() {
+    return {
+      func: "",
+      A: "",
+      B: "",
+      N: ""
+    };
+  },
+  methods: {
+    process: function() {
+      console.log(
+        window.integral.trap(
+          window.mathjs.evaluate(this.A),
+          window.mathjs.evaluate(this.B),
+          window.mathjs.evaluate(this.N),
+          window.mathjs.evaluate(this.func)
+        )
+      );
+      console.log(this.A);
+      console.log(this.B);
+      console.log(this.N);
     }
-}
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
